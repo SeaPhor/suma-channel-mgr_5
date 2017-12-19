c5872f35775076eb57c7e8c42022aafa100854e4
Shawn Miller <shawn.miller@academy.com>
2017-12-19 10:23:22 -0600

    Working on Tools/5_promotesuma.sh for testing

--- Tools/5_promotesuma.sh
+++ Tools/5_promotesuma.sh
@@ -4,7 +4,7 @@
 WRKDIR=~/SUSEManager
 cd $WRKDIR
 git pull origin master
-#
+# Set Global Variables
 NEWREV=$(( `ls -1 $WRKDIR | grep ^5 | tail -n1 | awk -F- '{print $2}'` + 1 ));
 BASENAME="`ls -1 $WRKDIR | grep ^5 | tail -n1 | awk -F- '{print $1}'`"
 ARCHFIL="`ls -1 $WRKDIR | grep ^5 | head -n1`"
@@ -20,14 +20,15 @@ mv $WRKDIR/$ARCHFIL $WRKDIR/Archive/.
 mv $WRKDIR/tempfil $WRKDIR/$BASENAME-$NEWREV
 mv $WRKDIR/$BASENAME-$NEWREV/* $WRKDIR/$BASENAME-$NEWREV/$VERNAME\_5.0.0-$NEWREV
 sed -i s/SCRIPT_RELEASE\=\"5.0.0-..\"/SCRIPT_RELEASE\=\"5.0.0-$NEWREV\"/g $WRKDIR/$BASENAME-$NEWREV/$VERNAME\_5.0.0-$NEWREV
-# Push promotion to github repository and exit [will not work, need to add syntax to apply user and pass]
+# add, commit, and push changes to github
 if [[ -a ~/.githubrepo ]]; then
   source ~/.githubrepo
   cd $WRKDIR
   CPFIL="`ls -1 $WRKDIR | grep ^5 | tail -n2 | head -n1`"
-  git add *
-  git commit -a -m "$CPFIL is now Latest_Stable - Promoted to 5.0.0-$NEWREV on $REVDATE"
-  git push https://$MYUSER:$MYPASS@github.com/seaphor-wbc/SUSEManager.git
+# Commenting out commits and push for testing purpose- remove this line and all '#' from the beginning of the following lines.
+#  git add *
+#  git commit -a -m "$CPFIL is now Latest_Stable - Promoted to 5.0.0-$NEWREV on $REVDATE"
+#  git push https://$MYUSER:$MYPASS@github.com/seaphor-wbc/SUSEManager.git
 #  git push origin master
 fi
 exit 0
