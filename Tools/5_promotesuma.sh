58fe77865337c02e94b5c0dc25299191447f93b3
root <root@suma3.woodbeeco.com>
2017-12-23 12:20:47 -0600

    Final fork testing and promotions complete Latest Stable is 03

--- Tools/5_promotesuma.sh
+++ Tools/5_promotesuma.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Set working directory and refresh repository
 #WRKDIR=/home/smiller/MyNewRepos/GitHub/SUSEManager
-WRKDIR=~/SUSEManager
+WRKDIR=~/suma-channel-mgr_5
 cd $WRKDIR
 git pull origin master
 # Set Global Variables
+ls -1 $WRKDIR | grep ^5 | tail -n1 | awk -F- '{print $2}' > /tmp/tmp.txt
 NEWREV=$(( `ls -1 $WRKDIR | grep ^5 | tail -n1 | awk -F- '{print $2}'` + 1 ));
 BASENAME="`ls -1 $WRKDIR | grep ^5 | tail -n1 | awk -F- '{print $1}'`"
 ARCHFIL="`ls -1 $WRKDIR | grep ^5 | head -n1`"
@@ -17,9 +18,22 @@ cp -r $WRKDIR/$LATEFIL $WRKDIR/tempfil
 sed -i "s/SCRIPT_RELEASE_DATE\=\"?? ??? 201?\"/SCRIPT_RELEASE_DATE\=\"$REVDATE\"/g" $WRKDIR/$LATEFIL/*
 cp $WRKDIR/$LATEFIL/* $WRKDIR/Latest_Stable/$SCRPTNAME
 mv $WRKDIR/$ARCHFIL $WRKDIR/Archive/.
+if [[ "`cat /tmp/tmp.txt | grep ^0`" != "" ]]; then
+	NEWREV=0$NEWREV
+	#mv $WRKDIR/tempfil $WRKDIR/$BASENAME-0$NEWREV
+	#NEXTFILE=$WRKDIR/$BASENAME-0$NEWREV
+	#mv $NEXTFILE/* $NEXTFILE/$VERNAME\_5.0.0-0$NEWREV
+	#sed -i s/SCRIPT_RELEASE\=\"5.0.0-..\"/SCRIPT_RELEASE\=\"5.0.0-0$NEWREV\"/g $NEXTFILE/$VERNAME\_5.0.0-0$NEWREV
+#elsei
+	#mv $WRKDIR/tempfil $WRKDIR/$BASENAME-$NEWREV
+	#NEXTFILE=$WRKDIR/$BASENAME-$NEWREV
+	#mv $NEXTFILE/* $NEXTFILE/$VERNAME\_5.0.0-$NEWREV
+	#sed -i s/SCRIPT_RELEASE\=\"5.0.0-..\"/SCRIPT_RELEASE\=\"5.0.0-$NEWREV\"/g $NEXTFILE/$VERNAME\_5.0.0-$NEWREV
+fi
 mv $WRKDIR/tempfil $WRKDIR/$BASENAME-$NEWREV
-mv $WRKDIR/$BASENAME-$NEWREV/* $WRKDIR/$BASENAME-$NEWREV/$VERNAME\_5.0.0-$NEWREV
-sed -i s/SCRIPT_RELEASE\=\"5.0.0-..\"/SCRIPT_RELEASE\=\"5.0.0-$NEWREV\"/g $WRKDIR/$BASENAME-$NEWREV/$VERNAME\_5.0.0-$NEWREV
+NEXTFILE=$WRKDIR/$BASENAME-$NEWREV
+mv $NEXTFILE/* $NEXTFILE/$VERNAME\_5.0.0-$NEWREV
+sed -i s/SCRIPT_RELEASE\=\"5.0.0-..\"/SCRIPT_RELEASE\=\"5.0.0-$NEWREV\"/g $NEXTFILE/$VERNAME\_5.0.0-$NEWREV
 # add, commit, and push changes to github
 if [[ -a ~/.githubrepo ]]; then
   source ~/.githubrepo
